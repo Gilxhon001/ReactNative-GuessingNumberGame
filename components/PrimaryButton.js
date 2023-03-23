2193f2e15b08764af6fb3a07fc1be8e3340280d9
Gilxhon <gilxhonhima@gmail.com>
2023-03-23 15:36:51 +0100

    add structure and style

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,5 +1,44 @@
-function PrimaryButton() {
+import {StyleSheet, Text, View, Pressable} from "react-native";
 
+function pressHandler() {
+    console.log('Pressed!');
 }
 
-export default PrimaryButton;
+function PrimaryButton({children}) {
+    return (
+        <View style={styles.buttonOuterContainer}>
+            <Pressable
+                onPress={pressHandler}
+                style={({pressed}) => pressed ? [styles.buttonInnerContainer, styles.pressed] : styles.buttonInnerContainer}
+                android_ripple={{color: '#916a78'}}
+
+            >
+                <Text style={styles.buttonText}>{children}</Text>
+            </Pressable>
+        </View>
+    );
+}
+
+export default PrimaryButton
+
+const styles = StyleSheet.create({
+    buttonOuterContainer: {
+        borderRadius: 28,
+        margin: 5,
+        overflow: "hidden",
+    },
+    buttonInnerContainer: {
+        backgroundColor: '#72063C',
+        paddingHorizontal: 16,
+        paddingVertical: 8,
+        elevation: 2,
+    },
+    buttonText: {
+        color: '#ddb52f',
+        textAlign: "center",
+    },
+    pressed: {
+        opacity: 0.75,
+    },
+});
+
